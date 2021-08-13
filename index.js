d092f864ffdf2cfba0e21d791f7b77c325b23b43
Jordan Diamond <81628102+JordanLeeDiamond@users.noreply.github.com>
2021-08-13 11:39:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const rules = document.getElementById("rules");
 const d1 = document.getElementById("d1");
 const player1 = document.getElementById("player1")
 const score = document.getElementById("score");
-// const player2 = document.getElementById("player2");
-// const score2 = document.getElementById("score2")
 const rollButton = document.getElementById("roll");
-// const addPlayer = document.getElementById("addPlayer")
 const resetButton = document.getElementById("reset");
 
 let p1Score = 0;
-
-// let p2Score = 0;
-
-// let playerCount = 1;
-// let currentPlayer = "player1"
 
 rollButton.addEventListener("click", () => {
     const diceRoll = Math.floor(Math.random() * 6 + 1)
@@ -30,13 +22,6 @@ resetButton.addEventListener("click", () => {
     score.innerHTML = `SCORE: 0`;
     rules.innerHTML = "Score 21 to Win!";
 })
-
-// addPlayer.addEventListener("click", () => {
-//     currentPlayer = "player1"
-//     playerCount = 2;
-//     player2.style.display = "block"
-//     resetButton()
-// })
 
     if (diceRoll != 1) {
         
